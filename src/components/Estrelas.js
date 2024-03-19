82dd0998d21123953f1811c6a5285135bb9488c0
dudurb17 <eduardo.robettibedin@gmail.com>
2024-03-19 10:47:07 -0300

    mudando as avaliações

--- src/components/Estrelas.js
+++ src/components/Estrelas.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import estrela from "../assets/estrela.png";
 import estrelaCinza from "../assets/estrelaCinza.png";
 
@@ -8,11 +8,34 @@ const Estrelas = ({
   editavel = false,
   grande = false,
 }) => {
+  const [quantidade, setQuantidade] = useState(quantidadeAntiga);
   const estilos = estilosFuncao(grande);
+
+  const getImage = (index) => {
+    if (index < quantidade) {
+      return estrela;
+    }
+    return estrelaCinza;
+  };
+
+  const RenderEstrelas = () => {
+    const listaEstrelas = [];
+    for (let i = 0; i < 5; i++) {
+      listaEstrelas.push(
+        <TouchableOpacity
+          key={i}
+          onPress={() => setQuantidade(i + 1)}
+          disabled={!editavel}
+        >
+          <Image source={getImage(i)} style={estilos.estrela} />
+        </TouchableOpacity>
+      );
+    }
+    return listaEstrelas;
+  };
   return (
     <View style={estilos.estrelas}>
-      <Image source={estrela} style={estilos.estrela} />
-      <Image source={estrela} style={estilos.estrela} />
+      <RenderEstrelas />
     </View>
   );
 };
